cdd6b960b7419febb5d0f1103442b447149f70f9
CesarLinux <eduardomelchor34@gmail.com>
2024-12-25 09:13:49 -0600

    auth pages roung

---
+++ src/app/centro/centro.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CentroRoutingModule } from './centro-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CentroRoutingModule
+  ]
+})
+export class CentroModule { }
