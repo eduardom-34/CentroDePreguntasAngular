78ce0d2fdbe220aa636ec93ec1686c0c49500673
CesarLinux <eduardomelchor34@gmail.com>
2024-12-25 11:13:00 -0600

    login screen

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
-  imports: [
+  exports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+
   ]
 })
 export class MaterialModule { }
