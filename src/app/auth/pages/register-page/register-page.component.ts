2982ad168e8c6d3bfc149259c27f9b2da445d8ce
CesarLinux <eduardomelchor34@gmail.com>
2024-12-25 23:34:47 -0600

    crear cuenta nueva

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Register } from '../../interfaces/register.interface';
+import { Router } from '@angular/router';
+import { SharedService } from '../../../shared/shared.service';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +11,44 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private sharedService: SharedService
+  ) {
+
+    this.myForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onRegister() {
+    if (this.myForm.invalid) {
+      this.sharedService.showSnackbar("Por favor ingrese sus credenciales", "Error");
+      return;
+    }
+
+    const register: Register = {
+      firstName: this.myForm.value.firstName,
+      lastName: this.myForm.value.lastName,
+      userName: this.myForm.value.userName,
+      password: this.myForm.value.password,
+    }
+
+    this.authService.register(register.firstName, register.lastName, register.userName, register.password)
+    .subscribe({
+      next: (resp) => {
+        this.router.navigate(['/centro']);
+        this.sharedService.showSnackbar("Cuenta creada", "Bienvenido");
+      }
+    })
+  }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { catchError, first, map, Observable, of, tap } from 'rxjs';
 import { Sesion } from '../interfaces/sesion.interface';
 import { ReactiveFormsModule } from '@angular/forms';
 import { User } from '../../centro/interfaces/user.interface';
@@ -46,6 +46,14 @@ export class AuthService {
   logout() {
     this.user = undefined;
     localStorage.clear();
+  }
+
+  register(firstName: string, lastName: string, userName: string, password: string): Observable<Sesion> {
+    return this.http.post<Sesion>(`${this.baseUrl}/user`, {firstName, lastName, userName, password})
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', user.token))
+      )
   }
 
 }
