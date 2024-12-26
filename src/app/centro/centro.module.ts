3ec33c78bf8764d52aa2f99dcb336251f6b417d3
CesarLinux <eduardomelchor34@gmail.com>
2024-12-25 23:47:59 -0600

    mostrar username

--- src/app/centro/centro.module.ts
+++ src/app/centro/centro.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { CentroRoutingModule } from './centro-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { CentroPageComponent } from './pages/centro-page/centro-page.component';
 import { PreguntasPageComponent } from './pages/preguntas-page/preguntas-page.component';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
@@ -13,7 +12,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     LayoutPageComponent,
-    CentroPageComponent,
     PreguntasPageComponent
   ],
   imports: [
--- src/app/centro/pages/layout-page/layout-page.component.ts
+++ src/app/centro/pages/layout-page/layout-page.component.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
 import { Router } from '@angular/router';
 import { SharedService } from '../../../shared/shared.service';
+import { Sesion } from '../../../auth/interfaces/sesion.interface';
+import { User } from '../../interfaces/user.interface';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css'
 })
-export class LayoutPageComponent {
+export class LayoutPageComponent implements OnInit{
+
+  public user?: User;
+  public sesion!: Sesion;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private sharedService: SharedService
+    private sharedService: SharedService,
+    private userService: UserService
   ){}
+
+  ngOnInit(): void {
+
+
+    const currentUser = this.authService.currentUser;
+
+    currentUser === undefined ? null : this.sesion = currentUser;
+
+    this.userService.getByUserName(this.sesion.userName)
+    .subscribe( user => this.user = user )
+
+  }
+
+  getName() {
+    if( this.user === undefined) return ""
+    if( this.user.userName === undefined) return " "
+
+    return this.user.userName;
+  }
 
 
   onLogout(): void {
